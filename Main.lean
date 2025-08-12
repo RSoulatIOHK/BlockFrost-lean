38882eb328ded582f3bcfa2ec6f81dae3cf3f83a
RSoulatIOHK <romain.soulat@iohk.io>
2025-08-11 19:59:18 -0700

    chore: starting to write all tests from BF doc

--- Main.lean
+++ Main.lean
@@ -13,26 +13,118 @@ def main : IO Unit := do
   let env : Env := { base := "https://cardano-mainnet.blockfrost.io/api/v0", projectId := pid }
 
   BF.run env do
-    -- root check
-    let r : BFRoot ← Blockfrost.Typed.root
-    IO.println s!"Root: {r}"
+    if false then
+      -- THIS IS USED TO SKIP THE API CALLS THAT WORK
+      IO.println "Running the Blockfrost API Documentation examples"
 
-    -- health check
-    let h : BFHealth ← Blockfrost.Typed.health
-    IO.println s!"Health: {h}"
+      IO.println "GET /"
+      let root ← Blockfrost.Typed.root
+      IO.println root
 
-    -- health.clock check
-    let c : BFClock ← Blockfrost.Typed.health.clock
-    IO.println s!"Clock: {c}"
+      IO.println "GET /health"
+      let health ← Blockfrost.Typed.health
+      IO.println health
 
-   -- metrics
-    let m : Array BFMetrics ← Blockfrost.Typed.metrics
-    IO.println s!"Metrics: {m}"
+      IO.println "GET /health/clock"
+      let clock ← Blockfrost.Typed.health.clock
+      IO.println clock
 
-    -- metrics/endpoints
-    let e : Array BFEndpoints ← Blockfrost.Typed.metrics.endpoints
-    IO.println s!"Endpoints: {e}"
+      IO.println "GET /metrics"
+      let metrics ← Blockfrost.Typed.metrics
+      IO.println metrics
 
-    -- curl https://cardano-mainnet.blockfrost.io/api/v0/addresses/addr1qxqs59lphg8g6qndelq8xwqn60ag3aeyfcp33c2kdp46a09re5df3pzwwmyq946axfcejy5n4x0y99wqpgtp2gd0k09qsgy6pz/txs \
-    let addr : BFAddress ← Blockfrost.Typed.addresses.byAddress "addr1qxqs59lphg8g6qndelq8xwqn60ag3aeyfcp33c2kdp46a09re5df3pzwwmyq946axfcejy5n4x0y99wqpgtp2gd0k09qsgy6pz"
-    IO.println s!"Address: {addr}"
+      IO.println "GET /metrics/endpoints"
+      let endpoints ← Blockfrost.Typed.metrics.endpoints
+      IO.println endpoints
+
+      let stakeAddress := "stake1u9ylzsgxaa6xctf4juup682ar3juj85n8tx3hthnljg47zctvm3rc"
+
+      IO.println "GET /accounts/{stake_address}"
+      let account ← Blockfrost.Typed.accounts.byStake stakeAddress
+      IO.println account
+    else
+      -- ACCOUNTS : TODO
+
+      -- IO.println "GET /accounts/{stake_address}/rewards"
+      -- let rewards ← Blockfrost.Typed.accounts.rewards stakeAddress
+      -- IO.println rewards
+
+      -- IO.println "GET /accounts/{stake_address}/history"
+      -- let history ← Blockfrost.Typed.accounts.history stakeAddress
+      -- IO.println history
+
+      -- IO.println "GET /accounts/{stake_address}/delegations"
+      -- let delegations ← Blockfrost.Typed.accounts.delegations stakeAddress
+      -- IO.println delegations
+
+      -- IO.println "GET /accounts/{stake_address}/registrations"
+      -- let registrations ← Blockfrost.Typed.accounts.registrations stakeAddress
+      -- IO.println registrations
+
+      -- IO.println "GET /accounts/{stake_address}/withdrawals"
+      -- let withdrawals ← Blockfrost.Typed.accounts.withdrawals stakeAddress
+      -- IO.println withdrawals
+
+      -- IO.println "GET /accounts/{stake_address}/mirs"
+      -- let mirs ← Blockfrost.Typed.accounts.mirs stakeAddress
+      -- IO.println mirs
+
+      -- IO.println "GET /accounts/{stake_address}/addresses"
+      -- let addresses ← Blockfrost.Typed.accounts.addresses stakeAddress
+      -- IO.println addresses
+
+      -- IO.println "GET /accounts/{stake_address}/assets"
+      -- let assets ← Blockfrost.Typed.accounts.assets stakeAddress
+      -- IO.println assets
+
+      -- IO.println "GET /accounts/{stake_address}/addresses/total"
+      -- let totalAddresses ← Blockfrost.Typed.accounts.totalAddresses stakeAddress
+      -- IO.println totalAddresses
+
+      -- IO.println "GET /accounts/{stake_address}/utxos
+      -- let utxos ← Blockfrost.Typed.accounts.utxos stakeAddress
+      -- IO.println utxos
+
+      -- ADDRESSES
+      let addressForTest := "addr1qxqs59lphg8g6qndelq8xwqn60ag3aeyfcp33c2kdp46a09re5df3pzwwmyq946axfcejy5n4x0y99wqpgtp2gd0k09qsgy6pz"
+
+      IO.println "GET /addresses/{address}"
+      let address ← Blockfrost.Typed.addresses.byAddress addressForTest
+      IO.println address
+
+      -- IO.println "GET /addresses/{address}/extended"
+      -- let extendedInfoAddress ← Blockfrost.Typed.addresses.extended addressForTest
+      -- IO.println extendedInfoAddress
+
+      -- IO.println "GET /addresses/{address}/total"
+      -- let totalAddress ← Blockfrost.Typed.addresses.total addressForTest
+      -- IO.println totalAddress
+
+      IO.println "GET /addresses/{address}/utxos"
+      let utxos ← Blockfrost.Typed.addresses.utxos addressForTest
+      IO.println utxos
+
+      -- IO.println "GET /addresses/{address}/utxos/{asset}"
+      -- let utxoAsset ← Blockfrost.Typed.addresses.utxoAsset addressForTest
+      -- IO.println utxoAsset
+
+      -- DEPRECATED GET /addresses/{address}/txs
+
+      -- IO.println "GET /addresses/{address}/transactions"
+      -- let transactions ← Blockfrost.Typed.addresses.transactions addressForTest
+      -- IO.println transactions
+
+      -- ASSETS
+      IO.println "GET /assets"
+      let assets ← Blockfrost.Typed.assets
+      IO.println assets
+
+      let assetForTest := "b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727952756279"
+
+      IO.println "GET /assets/{asset}"
+      let assetInfo ← Blockfrost.Typed.assets.byAsset assetForTest
+      IO.println assetInfo
+
+      -- IO.println "GET /assets/{asset}/history"
+      -- let assetHistory ← Blockfrost.Typed.assets.history assetForTest
+      -- IO.println assetHistory
