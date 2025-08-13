bc688ee2a97f6e765ed18755651e1afe5134d30c
RSoulatIOHK <romain.soulat@iohk.io>
2025-08-12 17:22:31 -0700

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,14 +1,14 @@
 import Lake
 open Lake DSL System
 
-package «curl-ffi-demo»
+package «blockfrost-lean4»
 
 lean_lib «CurlFFI»
-
 lean_lib «Blockfrost»
-
 lean_lib «Tests»
--- Build the C wrapper and link with Homebrew curl
+lean_lib «Examples»
+
+-- Build the C wrapper and link with curl
 extern_lib «http_client» pkg := do
   let name   := nameToStaticLib "http_client"
   let cDir   := pkg.dir / "c"
@@ -20,18 +20,19 @@ extern_lib «http_client» pkg := do
   let cflags := #[
     "-fPIC", "-O2",
     "-I", (← getLeanIncludeDir).toString,
-    "-I", "/opt/homebrew/opt/curl/include"  -- <= your brew prefix
+    "-I", "/opt/homebrew/opt/curl/include" -- Need to check where it gets otherwise
   ]
+
   let srcJob ← inputFile src false
   let oJob   ← buildO oFile srcJob cflags
   buildStaticLib (pkg.staticLibDir / name) #[oJob]
 
 @[default_target]
-lean_exe «demo» {
+lean_exe «blockfrost-test» {
   root := `Main
   moreLinkArgs := #[
-    "-L", "/opt/homebrew/opt/curl/lib",      -- <= your brew prefix
+    "-L", "/opt/homebrew/opt/curl/lib",
     "-lcurl",
-    "-Wl,-rpath,/opt/homebrew/opt/curl/lib"  -- ensure it’s found at runtime
+    "-Wl,-rpath,/opt/homebrew/opt/curl/lib"
   ]
 }
